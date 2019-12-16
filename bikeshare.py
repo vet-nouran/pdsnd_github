569f9b1406d5604c4f67e9d72ae5214bcad2a6b3
ahmedeweed <xmega.mind10@gmail.com>
2019-12-16 19:51:14 +0200

    feat: clean some loops

--- bikeshare.py
+++ bikeshare.py
@@ -157,7 +157,7 @@ def time_statistics(df):
     print("\nThis took {} seconds.".format((time.time() - start_time)))
     print('-'*40)
 
-def station_stats(df):
+def station_statistics(df):
     """Displays statistics on the most popular stations and trip."""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
@@ -262,7 +262,7 @@ def main():
         df = load_data(city, month, day)
 
         time_statistics(df)
-        station_stats(df)
+        station_statistics(df)
         trip_duration_stats(df)
         user_stats(df)
 
